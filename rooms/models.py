e765d407d92d5842a3430249701234373b8f975e
Hwang-Injun <sroo2315@gmail.com>
2020-04-12 20:20:34 +0900

    room model part1

--- rooms/models.py
+++ rooms/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+
 class Room(core_models.TimeStampedModel):
 
     """Room Model Definition """
@@ -15,7 +16,8 @@ class Room(core_models.TimeStampedModel):
     address = models.CharField(max_length=140)
     guests = models.IntegerField()
     beds = models.IntegerField()
-    baths = models.Integerfield()
+    baths = models.IntegerField()
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
