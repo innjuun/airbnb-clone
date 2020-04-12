10962714c247d304348c91e13b2fba373d3951fe
Hwang-Injun <sroo2315@gmail.com>
2020-04-12 20:12:06 +0900

    until lecture 4.0

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class TimeStampedModel(models.Model):
+
+    """ Time Stamped Model """
+
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
