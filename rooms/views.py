59d9c10725189c99cff902617357514531b2c0c9
Hwang-Injun <sroo2315@gmail.com>
2020-04-30 00:51:49 +0900

    13.3 Amenities and Facilities Form

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,8 @@
 
 from django.views.generic import ListView, DetailView
-from django.http import Http404
-from django.urls import reverse
 from django.shortcuts import render
+
+from django_countries import countries
 from . import models
 
 """
@@ -49,6 +49,26 @@ class RoomDetail(DetailView):
     
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    form = {
+        "city": city, "s_room_type": room_type, "s_country": country, 
+        "price": price, "guests": guests, "bedrooms": bedrooms,
+        "beds": beds, "baths": baths,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    choices = {"countries": countries, "room_types": room_types,
+               "amenities": amenities, "facilities": facilities}
+
+
+    return render(request, "rooms/search.html", {**form, **choices})
