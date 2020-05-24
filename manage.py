4583389e61f8e015c7a3ba072a5529a9b1e613d6
Hwang-Injun <sroo2315@gmail.com>
2020-05-24 17:05:50 +0900

    16.1 Dotenv

--- manage.py
+++ manage.py
@@ -2,10 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,9 +14,10 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-         ) from exc
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    dotenv.read_dotenv()
     main()
