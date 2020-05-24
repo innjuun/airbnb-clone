bc66fb251c2fe2d5d22e17ca9b82cb08397736c5
Hwang-Injun <sroo2315@gmail.com>
2020-05-24 22:06:44 +0900

    16.4 Comepleting Verification

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from . import forms, models
 
 
 class LoginView(FormView):
@@ -62,3 +62,16 @@ class SignUpView(FormView):
         user.verify_email()
         return super().form_valid(form)
         # return super().form_vaild(form)
+
+
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_verified = True
+        user.email_secret = ""
+        user.save()
+        # todo : add success message
+    except models.User.DoesNotExist:
+        # todo : add error message
+        pass
+    return redirect(reverse("core:home"))
